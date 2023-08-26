a3a43c432009df3dd32e7578f3e6197f886c9d32
rabiul246 <hasanrabiul118@gmail.com>
2023-08-26 22:44:39 +0600

    classAdded

--- src/mainpkg/TransactionVendor.java
+++ src/mainpkg/TransactionVendor.java
@@ -4,8 +4,17 @@
  */
 package mainpkg;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.time.LocalDate;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 /**
  *
@@ -56,6 +65,71 @@ public class TransactionVendor implements Serializable {
         this.date = date;
     }
     
+    public boolean addTransactionReport() {
+        
+        File transactionFile = null;
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+
+        try {
+            transactionFile = new File("TransactionReportVendor.bin");
+            if(transactionFile.exists()) {
+                fos = new FileOutputStream(transactionFile, true);
+                oos = new AppendObjectOutputStream(fos);
+            }
+            else {
+                fos = new FileOutputStream(transactionFile);
+                oos = new ObjectOutputStream(fos);
+            }
+            oos.writeObject(this);
+            return true;
+        }
+        catch (FileNotFoundException e) {
+            return false;
+        }
+        catch(IOException e) {
+            return false;
+        }
+        finally {
+            try {
+                if(oos != null) oos.close();
+            }
+            catch(IOException e) {
+            }
+        }
+    }
     
+    
+    public static ObservableList<TransactionVendor> getTransactionReport() {
+        ObservableList<TransactionVendor> transaction = FXCollections.observableArrayList();
+        TransactionVendor tempInst;
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
+        
+        try{
+            File f1 = new File("TransactionReportVendor.bin");
+            fis = new FileInputStream(f1);
+            ois = new ObjectInputStream(fis);
+            while(true) {
+                tempInst = (TransactionVendor)ois.readObject();
+                transaction.add(tempInst);
+            }
+        }
+        catch(IOException e) { 
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if(ois != null) {
+                    ois.close();
+                }
+            }
+            catch(IOException e) {
+            }
+            return transaction;
+        }
+    }
     
 }
